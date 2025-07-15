cf11745590155e2b15c5ad762f8b5802a2b3ae1a
JJYCode <12975537+jjycode@user.noreply.gitee.com>
2025-07-15 15:16:06 +0800

    refactor(DataReplay): 优化了图标的UI显示，增加了光标悬浮显示数据，十字线，曲线高亮等功能

--- assets/__init__.py
+++ assets/__init__.py
@@ -12,5 +12,7 @@ ICON_CIRCLE = (_BASE_PATH / 'icon/circle.svg').as_posix()
 ICON_PLAY= (_BASE_PATH / 'icon/play.svg').as_posix()
 ICON_PAUSE= (_BASE_PATH / 'icon/pause.svg').as_posix()
 ICON_STOP= (_BASE_PATH / 'icon/stop.svg').as_posix()
-
+ICON_FOLDER_OPEN= (_BASE_PATH / 'icon/folder-open.svg').as_posix()
+ICON_BACKWARD= (_BASE_PATH / 'icon/backward.svg').as_posix()
+ICON_GLASS_CHART= (_BASE_PATH / 'icon/magnifying-glass-chart.svg').as_posix()
 
